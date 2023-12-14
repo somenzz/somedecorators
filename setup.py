874e4fe8c0f9073cfaa5449bff061d7a68712cb9
somenzz <somenzz.163.com>
2023-12-14 11:09:53 +0800

    add decorator wechat_on_exception

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ license = Path("LICENSE")
 # Read the version without importing the package
 # (and thus attempting to import packages it depends on that may not be
 # installed yet)
-version = "0.8"
+version = "0.9"
 
 NAME = 'somedecorators'
 VERSION = version
